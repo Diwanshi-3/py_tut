2b5d43cc1edf878c006efb59a2559c5bb2c0958a
Diwanshi-3 <diwanshichhabra5@gmail.com>
2025-12-25 09:48:57 +0530

    tyecasting concept.

---
+++ python/type_casting.py
@@ -0,0 +1,9 @@
+a='10'
+b=20
+# print(a+b) {This throws an error because we can only concatenate the strings not an integer to string hence we need typecasting concept there}
+print(int(a)+b) #Explicit typecasting of string a to integer  a
+c=10.5
+d=8
+print(c+d)#Implicit conversion
+
+
