3a7aa71bcc5b7de31e0dbbc3307b3504dd83b842
Diwanshi-3 <diwanshichhabra5@gmail.com>
2025-12-28 00:03:23 +0530

    Shorthand if else statement

---
+++ python/Shorthand_if_else.py
@@ -0,0 +1,12 @@
+a=300
+b=33002
+'''
+a>b:
+    print("A")
+elif a==b:
+    print("=")
+else:
+    print("B")
+'''
+#Shorthand if else of above example is..
+print("A")if a>b else print("=") if a==b else print("B")
